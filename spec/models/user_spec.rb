9d77a513ef70743f47cb2c373051f6ae925ed0a7
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-10-04 18:09:22 +0900

    新規登録機能のテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe User do
-    context '名前が空白の時はユーザー登録ができない' do
-      it "is invalid without a nickname" do
-        user = User.new(name: "", email: "aaa@gmail.com", password: "00000000", password_confirmation: "00000000")
-        expect(user).not_to be_valid
-      end
+  describe "バリデーション成功" do
+    it "ユーザー登録ができる" do
+      user = build(:user)
+      expect(user).to be_valid
     end
   end
+end
 
-  describe User do
-    context 'emailが空白の時はユーザー登録ができない' do
-      it "is invalid without an email" do
-        user = User.new(name: "test", email: "", password: "00000000", password_confirmation: "00000000")
-        expect(user).not_to be_valid
-      end
+RSpec.describe User, type: :model do
+  describe "バリデーション失敗" do
+    it "名前が空白の時はユーザー登録ができない" do
+      user = build(:user, name: "")
+      expect(user).not_to be_valid
+    end
+
+    it "名前が16文字以上の時はユーザー登録ができない" do
+      user = build(:user, name: "a" * 16)
+      expect(user).not_to be_valid
+    end
+
+    it "emailが空白の時はユーザー登録ができない" do
+      user = build(:user, email: "")
+      expect(user).not_to be_valid
+    end
+
+    it "パスワードが7文字以下の時はユーザー登録ができない" do
+      user = build(:user, password: "a" * 7, password_confirmation: "a" * 7)
+      expect(user).not_to be_valid
+    end
+
+    it "確認用パスワードが空白の時はユーザー登録ができない" do
+      user = build(:user, password_confirmation: "")
+      expect(user).not_to be_valid
     end
   end
 end
