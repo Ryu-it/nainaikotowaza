9d6789c9c66f36e21b82a3a748dd00b97c7b5484
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-10-13 14:12:20 +0900

    add:フォロー機能のテスト

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "フォローが成功する" do
+    let(:user) { create(:user, :with_active_follows) }
+    it "フォローできること" do
+      expect(user.active_follows.count).to eq(1)
+    end
+  end
+
+  describe "フォローが失敗する" do
+    let(:user) { create(:user) }
+    it "自分自身をフォローできないこと" do
+      follow = build(:follow, follower: user, followed: user)
+      expect(follow).not_to be_valid
+    end
+
+    it "同じユーザーを重複してフォローできないこと" do
+      other_user = create(:user)
+      create(:follow, follower: user, followed: other_user)
+      duplicate_follow = build(:follow, follower: user, followed: other_user)
+      expect(duplicate_follow).not_to be_valid
+    end
+  end
 end
---
+++ spec/system/follows_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "follows", type: :system do
+  let(:user) { create(:user) }
+  before do
+    user
+    visit new_user_session_path
+    fill_in "Eメール", with: user.email
+    fill_in "パスワード", with: "12345678"
+    click_button "Log in"
+    expect(page).to have_content("ログインしました") # まずはここで保証
+  end
+
+  describe "フォローの確認" do
+    scenario "フォローした遷移先はその場" do
+      save_and_open_page
+      other_user = create(:user)
+      visit users_path
+      within("form") do
+        fill_in "q_name_cont", with: other_user.name
+        find_field("q_name_cont").send_keys(:enter)
+      end
+      click_link "フォロー"
+      expect(page).to have_current_path(users_path, ignore_query: true)
+    end
+  end
+end
