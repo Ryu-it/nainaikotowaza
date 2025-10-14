df6772286208ba34a74c75b7581e06442e713731
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-10-14 23:28:14 +0900

    add:通知が来ると件数が横に表示される

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_unread_notifications_count, if: :user_signed_in?
+
+  private
+  # notificationsテーブルのfalseレコードを取得してカウント
+  def set_unread_notifications_count
+    @unread_notifications_count = current_user.passive_notifications.unread.count
+  end
 end
