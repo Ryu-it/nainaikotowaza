cf806ad811c055b1ffeea92d53cd3c9cfe34494b
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-10-15 22:56:09 +0900

    add:ことわざを作る部屋を作成しました

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,26 @@ class RoomsController < ApplicationController
     # 検索をした時だけ表示させる
     @users = params[:q].present? ? @q.result(distinct: true) : User.none
   end
+
+  def create
+    # 失敗した時にロールバックするためにトランザクションを使用
+    user = User.find_by!(name: params.dig(:room, :user_name))
+    ActiveRecord::Base.transaction do
+      @room = Room.new(room_params)
+      @room.save!
+      # RoomUserを2つ作成する
+      @room.room_users.create!(user: current_user, role: :word_giver)
+      @room.room_users.create!(user: user, role: :proverb_maker)
+    end
+    redirect_to new_room_proverb_path(@room), notice: "ルームを作成しました"
+
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:alert] = "ルームの作成に失敗しました"
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name).merge(owner: current_user)
+  end
 end
---
+++ app/helpers/rooms/proverbs_helper.rb
@@ -0,0 +1,2 @@
+module Rooms::ProverbsHelper
+end
