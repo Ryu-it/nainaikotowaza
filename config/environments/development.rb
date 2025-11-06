d4b192d85ed844e3bf0b7717b4f3a943aa569321
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-11-06 15:05:32 +0900

    fix:assetsの開発環境を修正しました

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # 開発環境でのTailwind CSSの変更を即座に反映させる設定
+  config.assets.compile = true
+  config.assets.digest  = false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
