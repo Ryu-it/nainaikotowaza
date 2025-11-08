ea12aff90c12a3189818161cdecb3543d6aa2092
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-11-08 21:50:36 +0900

    add:リアクションのviewのロジックをhelperに切りました

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -1,2 +1,17 @@
 module ReactionsHelper
+  # Structクラスを使って、リアクションの表示情報をまとめる
+  ReactionView = Struct.new(:existing, :active, :count, keyword_init: true)
+
+  def reaction_view(reactable, user, kind)
+    rel = reactable.reactions
+
+    # 未ログイン時
+    return ReactionView.new(existing: nil, active: false, count: rel.where(kind: kind).count) unless user
+
+    existing = rel.find_by(user:, kind:)
+    active   = existing.present?
+    count    = rel.where(kind: kind).count
+
+    ReactionView.new(existing:, active:, count:)
+  end
 end
