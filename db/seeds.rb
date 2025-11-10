3f8ba49a04c474e0128c9172279bc4c9f45392c2
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-11-10 15:54:01 +0900

    add:ページネーション機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,33 @@ require "faker"
 
 return unless Rails.env.development? # 本番で実行されないようにする
 
-# fakerを使ってユーザーを10人作成する
-10.times do
+# ---- ユーザーを10人作成して配列で受け取る ----
+users = 10.times.map do
   User.create!(
-    name: Faker::Name.name,
+    name:  Faker::Name.name,
     email: Faker::Internet.unique.email,
     password: "password",
     password_confirmation: "password"
   )
+end
+
+# fakerを使ってことわざを20個作成する
+20.times do
+  proverb = Proverb.create!(
+    word1: Faker::Verb.base.capitalize,
+    word2: Faker::Emotion.noun.capitalize,
+    title: Faker::Quote.famous_last_words,
+    meaning: Faker::Lorem.sentence(word_count: 10),
+    example: Faker::Lorem.sentence(word_count: 20),
+    status: :in_progress,
+    room: nil
+  )
+
+  # ランダムなユーザーを1人〜2人紐づける
+  rand(1..2).times do
+    ProverbContributor.create!(
+      proverb: proverb,
+      user: users.sample
+    )
+  end
 end
