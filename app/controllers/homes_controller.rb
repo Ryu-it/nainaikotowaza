0a274993c15e790a084a3f3b2284d9760bcc1960
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-11-14 09:36:01 +0900

    add:トップでタイトルから詳細に遷移

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,12 @@
 class HomesController < ApplicationController
   def index
-    @recent_proverb_titles = Proverb.order(created_at: :desc).limit(10).pluck(:title)
+    @recent_proverbs = Proverb.order(created_at: :desc).limit(10)
+
+    @recent_proverb_items = @recent_proverbs.map do |p|
+      {
+        title: p.title,
+        url:   proverb_path(p) # or proverb_url(p)（絶対URLが欲しければこっち）
+      }
+    end
   end
 end
