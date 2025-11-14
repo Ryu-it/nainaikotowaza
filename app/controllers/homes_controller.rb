c41bf55defd60dd3230a909288d1ee2a7ef0cc49
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-11-14 11:23:28 +0900

    add:トップにユーザー名

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,19 @@
 class HomesController < ApplicationController
   def index
-    @recent_proverbs = Proverb.order(created_at: :desc).limit(10)
+    @recent_proverbs = Proverb.titled.order(created_at: :desc).limit(10)
 
-    @recent_proverb_items = @recent_proverbs.map do |p|
-      {
-        title: p.title,
-        url:   proverb_path(p) # or proverb_url(p)（絶対URLが欲しければこっち）
-      }
-    end
+    @recent_proverb_items =
+      @recent_proverbs.includes(proverb_contributors: :user).map do |p|
+        user_names =
+          p.proverb_contributors
+           .map { |pc| pc.user.name }
+           .uniq
+
+        {
+          title: p.title,
+          url: proverb_path(p),
+          users: user_names
+        }
+      end
   end
 end
