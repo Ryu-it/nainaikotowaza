c5fb91a13ece6713823440abcc4f825e04459e6f
西田龍一 <nishidaryuuichi@nishidaryuuichinoMacBook-Air.local>
2025-11-16 14:32:09 +0900

    add:通知を自動的に既読にする

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,23 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :mark_invitations_as_read, only: :index
 
   def index
     @notifications = current_user.passive_notifications
                                  .invitations
                                  .order(created_at: :desc)
                                  .page(params[:page]).per(10)
+  end
+
+  private
+
+  def mark_invitations_as_read
+    current_user.passive_notifications
+                .invitations
+                .unread
+                .update_all(is_checked: true)
+
+    # 既読後のカウントにする(ヘッダーのカウント更新のため)
+    set_unread_invitations_count
   end
 end
